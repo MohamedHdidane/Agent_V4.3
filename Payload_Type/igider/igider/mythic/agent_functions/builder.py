1fdf7f52bde10a792406225360b2ad2c41c52fe2
MohamedHdidane <mohamed642hdidane@gmail.com>
2025-06-21 23:07:31 +0100

    priv_esc win

--- Payload_Type/igider/igider/mythic/agent_functions/builder.py
+++ Payload_Type/igider/igider/mythic/agent_functions/builder.py
@@ -409,8 +409,8 @@ class Igider(PayloadType):
             command_code = ""
             selected_os = self.selected_os.lower()
             for cmd in self.commands.get_commands():
-                cmd_class = self.commands.command_dict[cmd]
-                is_platform_specific = getattr(cmd_class.attributes, "is_platform_specific", False)
+                cmd_class = self.commands.getcommand(cmd)
+                is_platform_specific = cmd_class.attributes.is_platform_specific if cmd_class else False
                 if is_platform_specific:
                     if selected_os == "windows":
                         platform_dir = self.agent_code_path / "windows"
